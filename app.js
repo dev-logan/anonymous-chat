1968012da7b00fc9d75fc2c35d2834f52f28e4f2
fancyers <kjng5901@gmail.com>
2022-01-31 22:46:02 +0900

    항해 대나무숲 미니프로젝트 시작

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+const userList = {}
+
+io.on('connection', (socket) => {
+    console.log(socket.id, '연결됨.')
+
+    socket.on('send users', (nickname) => {
+        userList[socket.id] = nickname
+        const nicknames = Object.values(userList)
+        io.emit('receive users', nicknames)
+    })
+
+    socket.on('send message', (name, text) => {
+        const msg = name + ': ' + text
+        console.log(msg)
+        io.emit('receive message', msg)
+    })
+
+    socket.on('disconnect', () => {
+        delete userList[socket.id]
+        const nicknames = Object.values(userList)
+        io.emit('receive users', nicknames)
+        console.log(socket.id, '해제됨.')
+    })    
+})
+
+http.listen(8080, () => {
+    console.log('8080 포트 ON')
+})
